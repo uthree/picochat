44fe2c69d4c3cfbb93b94f2e249d9a00b6a0b33f
uthree <uthree271828@gmail.com>
2026-06-28 03:46:00 +0900

    Update gpt.py

--- picochat/model/gpt.py
+++ picochat/model/gpt.py
@@ -18,8 +18,141 @@ def rotate_half(x: Tensor) -> Tensor:
 
 
 class SwiGLU(nn.Module):
-    pass
+    def __init__(
+        self, d_model: int, d_hidden: int | None = None, p_dropout: float = 0.1
+    ):
+        super().__init__()
+        self.p_dropout = p_dropout
+        if d_hidden is None:
+            d_hidden = d_model * 3
+        self.proj_up = nn.Linear(d_model, d_hidden)
+        self.proj_gate = nn.Linear(d_model, d_hidden)
+        self.proj_down = nn.Linear(d_hidden, d_model)
+
+    def forward(self, x: Tensor) -> Tensor:
+        x = rms_norm(x)
+        x = self.proj_up(x) * self.proj_gate(x).silu()
+        x = F.dropout(x, self.p_dropout)
+        x = self.proj_down(x)
+        x = F.dropout(x, self.p_dropout)
+        return x
 
 
-class CausalSelfAttention(nn.Module):
-    pass
+class SelfAttention(nn.Module):
+    def __init__(
+        self,
+        d_model: int,
+        n_heads: int,
+        n_groups: int | None = None,
+        rope_base: int = 10000,
+    ):
+        super().__init__()
+        self.rope_base = rope_base
+        self.n_heads = n_heads
+        self.d_model = d_model
+        self.d_head = d_model // n_heads
+        self.n_groups = n_heads if n_groups is None else n_groups
+        assert n_heads % self.n_groups == 0
+        assert d_model % n_heads == 0
+        self.proj_q = nn.Linear(d_model, self.d_head * n_heads, bias=False)
+        self.proj_k = nn.Linear(d_model, self.d_head * self.n_groups, bias=False)
+        self.proj_v = nn.Linear(d_model, self.d_head * self.n_groups, bias=False)
+        self.proj_o = nn.Linear(self.d_head * n_heads, d_model, bias=False)
+
+    def forward(self, x: Tensor, cache: Tensor | None = None) -> tuple[Tensor, Tensor]:
+        x = rms_norm(x)
+        query = rearrange(self.proj_q(x), "b l (h d) -> b h l d", d=self.d_head)
+        key = rms_norm(rearrange(self.proj_k(x), "b l (g d) -> b g l d", d=self.d_head))
+        value = rms_norm(
+            rearrange(self.proj_v(x), "b l (g d) -> b g l d", d=self.d_head)
+        )
+        if cache is not None:
+            key, value = (
+                torch.cat([cache[0], key], dim=-2),
+                torch.cat([cache[1], value], dim=-2),
+            )
+        cache = torch.stack([key, value])
+        offset = key.shape[-2] - query.shape[-2]
+        query, key = self._rope(query, offset=offset), self._rope(key)
+        attn = F.scaled_dot_product_attention(
+            query, key, value, is_causal=True, enable_gqa=True
+        )
+        y = self.proj_o(rearrange(attn, "b h l d -> b l (h d)"))
+        return y, cache
+
+    def _rope(self, x, offset: int = 0) -> Tensor:
+        if not hasattr(self, "sin"):
+            t = (torch.arange(self.rope_base) + offset)[:, None].float()
+            f = (
+                self.rope_base
+                ** (torch.linspace(0.0, 1.0, self.d_head // 2).repeat_interleave(2))
+            )[None, :]
+            theta = t / f
+            self.register_buffer("sin", torch.sin(theta))
+            self.register_buffer("cos", torch.cos(theta))
+        sin, cos = (
+            self.sin[offset : offset + x.shape[-2], :],
+            self.cos[offset : offset + x.shape[-2], :],
+        )
+        x = x * cos + rotate_half(x) * sin
+        return x
+
+
+class Transformer(nn.Module):
+    def __init__(
+        self,
+        d_model: int,
+        n_heads: int,
+        n_layers: int,
+        n_groups: int | None = None,
+        rope_base: int = 10000,
+        d_ffn: int | None = None,
+    ):
+        self.n_layers = n_layers
+        self.attn = nn.ModuleList(
+            [
+                SelfAttention(d_model, n_heads, n_groups=n_groups, rope_base=rope_base)
+                for _ in range(n_layers)
+            ]
+        )
+        self.ffn = nn.ModuleList(
+            [SwiGLU(d_model, d_hidden=d_ffn) for _ in range(n_layers)]
+        )
+
+    def forward(
+        self, x: Tensor, cache: list[Tensor | None] | None
+    ) -> tuple[Tensor, list[Tensor]]:
+        if cache is None:
+            cache = [None] * self.n_layers  # type: ignore
+        for i in range(self.n_layers):
+            s = x
+            x, cache[i] = self.attn(x, cache[i])  # type: ignore
+            x = x + s
+            s = x
+            x = self.ffn[i](x)
+            x = x + s
+        x = rms_norm(x)
+        return x, cache  # type: ignore
+
+
+class WordEmbedding(nn.Module):
+    def __init__(
+        self,
+        vocab_size: int,
+        d_model: int,
+        d_embed: int = 128,
+        n_prediction_heads: int = 4,  # for multi token prediction
+    ):
+        super().__init__()
+        self.embed = nn.Sequential(
+            nn.Embedding(vocab_size, d_embed), nn.Linear(d_embed, d_model, bias=False)
+        )
+        self.lmheads = nn.ModuleList(
+            [
+                nn.Sequential(
+                    nn.Linear(d_model, d_embed, bias=False),
+                    nn.Linear(d_embed, vocab_size, bias=False),
+                )
+                for _ in range(n_prediction_heads)
+            ]
+        )
